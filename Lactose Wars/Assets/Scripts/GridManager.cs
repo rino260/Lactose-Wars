e68b55e4541263a14282df0a987587504b629857
Ryan <rino262@gmail.com>
2020-03-02 22:54:36 -0500

    Re-factor tile visual generation to no longer be radial and instead selectively generate tiles from a square grid

--- Lactose Wars/Assets/Scripts/GridManager.cs
+++ Lactose Wars/Assets/Scripts/GridManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System;
 
 //The following code has been adapted from "Quill18creates"'s tile movement video tutorials series found here:
 //https://www.youtube.com/watch?v=kYeTW2Zr8NA
@@ -9,8 +10,10 @@ using System.Linq;
 
 public class GridManager : MonoBehaviour
 {
-    public int quadrantX;
-    public int quadrantY;
+    [Range(0,100)]
+    public int gridX;
+    [Range(0, 100)]
+    public int gridY;
 
     public TileType[] tileTypes;
     int[,] tileCoord;
@@ -18,15 +21,19 @@ public class GridManager : MonoBehaviour
     float xOffset = 0.866f;
     float zOffset = 0.75f;
 
-    bool nextColumn = false;
-    bool stop = false;
-    bool positiveX = true;
 
-    //Much like we do with the hex tiles, when implementing more than one unit give each one a script and when clicked on, we tell this script what unit is now selected (quill's civ/dungeon tile video #3 10:35)
+    //TODO Much like we do with the hex tiles, when implementing more than one unit give each one a script and when clicked on, we tell this script what unit is now selected (quill's civ/dungeon tile video #3 10:35)
     public GameObject selectedUnit;
 
     void Start()
     {
+        //Ensure that our grid will have an odd length and width so we will have a center tile on the grid
+        if (gridX % 2 == 0) { gridX++; }
+        if (gridY % 2 == 0) { gridY++; }
+
+        //Offset the grid manager according to the grid size and X & Z offsets
+        this.transform.position = new Vector3(this.transform.position.x - ((gridX / 2) * xOffset), this.transform.position.y, this.transform.position.y - ((gridY / 2) * zOffset));
+
         InitMapData();
         GeneratePathfindingGraph();
         GenerateMapTiles();
@@ -37,24 +44,13 @@ public class GridManager : MonoBehaviour
     //Set every tile to the desired type of tile in the "tiletype" array
     void InitMapData()
     {
-        tileCoord = new int[quadrantX, quadrantY];
+        tileCoord = new int[gridX, gridY];
 
-        //Initialize positive X tiles
-        for (int x = 0; x < quadrantX; x++)
+        //Initialize X tiles
+        for (int x = 0; x < gridX; x++)
         {
-            //Initialize positive Y tiles
-            for (int y = 0; y < quadrantY; y++) { tileCoord[x, y] = 0;}
-            //Initialize negative Y tiles
-            for (int y = -1; y > -quadrantY; y--) { tileCoord[x, -y] = 0;}
-        }
-
-        //Initialize negative X tiles
-        for (int x = -1; x > -quadrantX; x--)
-        {
-            //Initialize negative Y tiles
-            for (int y = -1; y > -quadrantY; y--) { tileCoord[-x, -y] = 0;}
-            //Initialize positive Y tiles
-            for (int y = 0; y < quadrantY; y++) { tileCoord[-x, y] = 0;}
+            //Initialize Y tiles
+            for (int y = 0; y < gridY; y++) { tileCoord[x, y] = 0;}
         }
     }
     
@@ -77,13 +73,13 @@ public class GridManager : MonoBehaviour
     void GeneratePathfindingGraph()
     {
         //Here we need to specify the maximum number of possible "Node" arrays
-        graph = new Node[quadrantX, quadrantY];
+        graph = new Node[gridX, gridY];
 
         //Initialize positive X nodes
-        for (int x = 0; x < quadrantX; x++)
+        for (int x = 0; x < gridX; x++)
         {
             //Initialize positive Y nodes
-            for (int y = 0; y < quadrantY; y++)
+            for (int y = 0; y < gridY; y++)
             {
                 //Here we need to actually create a node array that will contain the neighbors for each specific tile
                 graph[x, y] = new Node();
@@ -97,14 +93,14 @@ public class GridManager : MonoBehaviour
                         if (Mathf.Abs(y) % 2 == 1) { graph[x, y].neighbors.Add(graph[x, y - 1]); }
                         else { graph[x, y].neighbors.Add(graph[x - 1, y - 1]); }
                     }
-                    if (y < quadrantY - 1)
+                    if (y < gridY - 1)
                     {
                         if (Mathf.Abs(y) % 2 == 1) { graph[x, y].neighbors.Add(graph[x, y + 1]); }
                         else { graph[x, y].neighbors.Add(graph[x - 1, y + 1]); }
 
                     }
                 }
-                if (x < quadrantX - 1)
+                if (x < gridX - 1)
                 {
                     graph[x, y].neighbors.Add(graph[x + 1, y]);
 
@@ -113,33 +109,13 @@ public class GridManager : MonoBehaviour
                         if (Mathf.Abs(y) % 2 == 1) { graph[x, y].neighbors.Add(graph[x, y - 1]); }
                         else { graph[x, y].neighbors.Add(graph[x + 1, y - 1]); }
                     }
-                    if (y < quadrantY - 1)
+                    if (y < gridY - 1)
                     {
                         if (Mathf.Abs(y) % 2 == 1) { graph[x, y].neighbors.Add(graph[x, y + 1]); }
                         else { graph[x, y].neighbors.Add(graph[x + 1, y + 1]); }
 
                     }
                 } 
-            }
-            //Initialize negative Y nodes
-            for (int y = -1; y > -quadrantY; y--)
-            {
-
-            }
-        }
-
-        //Initialize negative X nodes
-        for (int x = -1; x > -quadrantX; x--)
-        {
-            //Initialize negative Y nodes
-            for (int y = -1; y > -quadrantY; y--)
-            {
-
-            }
-            //Initialize positive Y nodes
-            for (int y = 0; y < quadrantY; y++)
-            {
-
             }
         }
     }
@@ -148,13 +124,13 @@ public class GridManager : MonoBehaviour
     void AddNeighbors(int x, int y)
     {
         //As long as our x coordinate is greater than the minimum value of our negative x quadrant size:
-        if (x > -quadrantX + 1)
+        if (x > -gridX + 1)
         {
             //We can add a neighbor at our next x coordinate to our left
             graph[x, y].neighbors.Add(graph[x - 1, y]);
 
             //As long as our y coordinate is greater than the minimum value of our negative y quadrant size:
-            if (y > -quadrantY + 1)
+            if (y > -gridY + 1)
             {
                 //If we are on an odd row we can add a neighbor one y value below us on the current x coordinate (because the odd rows have their x values shifted and not the evens)
                 if (Mathf.Abs(y) % 2 == 1) { graph[x, y].neighbors.Add(graph[x, y - 1]); }
@@ -162,7 +138,7 @@ public class GridManager : MonoBehaviour
                 else { graph[x, y].neighbors.Add(graph[x - 1, y - 1]); }
             }
             //As long as our y coordinate is less than the maximum value of our negative y quadrant size:
-            if (y < quadrantY - 1)
+            if (y < gridY - 1)
             {
                 //If we are on an odd row we can add a neighbor one y value above us on the current x coordinate
                 if (Mathf.Abs(y) % 2 == 1) { graph[x, y].neighbors.Add(graph[x, y + 1]); }
@@ -172,13 +148,13 @@ public class GridManager : MonoBehaviour
             }
         }
         //As long as our x coordinate is less than the minimum value of our negative x quadrant size:
-        if (x < quadrantX - 1)
+        if (x < gridX - 1)
         {
             //We can add a neighbor at our next x coordinate to our right
             graph[x, y].neighbors.Add(graph[x + 1, y]);
 
             //As long as our y coordinate is greater than the minimum value of our negative y quadrant size:
-            if (y > -quadrantY + 1)
+            if (y > -gridY + 1)
             {
                 //If we are on an odd row we can add a neighbor one y value below us on the current x coordinate
                 if (Mathf.Abs(y) % 2 == 1) { graph[x, y].neighbors.Add(graph[x, y - 1]); }
@@ -186,7 +162,7 @@ public class GridManager : MonoBehaviour
                 else { graph[x, y].neighbors.Add(graph[x + 1, y - 1]); }
             }
             //As long as our y coordinate is less than the maximum value of our negative y quadrant size:
-            if (y < quadrantY - 1)
+            if (y < gridY - 1)
             {
                 //If we are on an odd row we can add a neighbor one y value above us on the current x coordinate
                 if (Mathf.Abs(y) % 2 == 1) { graph[x, y].neighbors.Add(graph[x, y + 1]); }
@@ -202,48 +178,11 @@ public class GridManager : MonoBehaviour
     //Increment the "quadNum" variable after generating each quadrant to prevent one quad from being generated more than once
     void GenerateMapTiles()
     {
-        if (positiveX)
+        for (int x = 0; x < gridX; x++)
         {
-            for (int x = 0; x < quadrantX; x++)
+            for (int y = 0; y < gridY; y++)
             {
-                //Spawn the positive Y tiles
-                for (int y = 0; y < quadrantY; y++)
-                {
-                    SpawnTiles(x, y, 0, tileTypes[tileCoord[x, y]].hexTilePrefab);
-
-                    if (nextColumn || stop) { break; }
-                }
-                //Spawn the negative Y tiles
-                for (int y = -1; y > -quadrantY; y--)
-                {
-                    SpawnTiles(x, y, -1, tileTypes[tileCoord[x, -y]].hexTilePrefab);
-
-                    if (nextColumn || stop) { break; }
-                }
-                if (stop) { break; }
-            }
-            positiveX = false;
-        }
-        
-        if(!positiveX)
-        {
-            for (int x = -1; x > -quadrantX; x--)
-            {
-                //Spawn the negative Y tiles
-                for (int y = -1; y > -quadrantY; y--)
-                {
-                    SpawnTiles(x, y, -1, tileTypes[tileCoord[-x, -y]].hexTilePrefab);
-
-                    if (nextColumn || stop) { break; }
-                }
-                //Spawn the positive Y tiles
-                for (int y = 0; y < quadrantY; y++)
-                {
-                    SpawnTiles(x, y, 0, tileTypes[tileCoord[-x, y]].hexTilePrefab);
-
-                    if (nextColumn || stop) { break; }
-                }
-                if (stop) { break; }
+                SpawnTiles(x, y, 0, tileTypes[tileCoord[x, y]].hexTilePrefab);
             }
         }
     }
@@ -251,10 +190,6 @@ public class GridManager : MonoBehaviour
 
     void SpawnTiles(int x, int y, float rowNum, GameObject tileCoord)
     {
-        //Reset loop checks at the start of the method so the next call will not be inaccurate
-        nextColumn = false;
-        stop = false;
-
         //Because our tiles are not a full unit in width we need to offset their x position slightly
         float xPos = x * xOffset;
 
@@ -262,27 +197,30 @@ public class GridManager : MonoBehaviour
         //We have to use the absolute value of Y so our negative Y quadrants will spawn properly
         if (Mathf.Abs(y) % 2 == 1) { xPos += xOffset / 2f; }
 
-        //Because we are importing our models from Blender they have a -90 degree X rotation applied
-        //Because of this we need to use the "z" axis like we would use the "y" axis if we want "y" to continue to be our "height" dimension
-        GameObject go = Instantiate(tileCoord, new Vector3(xPos, 0, y * zOffset), Quaternion.identity);
-        //We know each hex will have a script on it containing empty X and Y integers so when we instantiate a hex object, pass its X and Y data to each hex
-        HexData data = go.GetComponent<HexData>();
-        data.hexX = x;
-        data.hexY = y;
-        //Give each hex a name according to their grid coordinates and make them children of the grid master object for a cleaner heirarchy
-        go.name = "Hex (" + x + "," + y + ")";
-        go.transform.SetParent(transform);
-
-        //After spawning each tile, check if it is touching the map boundaries, if so, delete the tile and move to the next column of tile generation
+        //Create a layermask for our boundary objects
         int layerMask = 1 << 8;
-        Collider[] hitColliders = Physics.OverlapSphere(go.transform.position, 0.5f, layerMask);
-        if (hitColliders.Length > 0)
+        RaycastHit hit;
+        //Send a raycast downward from each potential coordinate point and only spawn a tile when it is inside the stage boundary
+        if (Physics.Raycast(new Vector3(transform.position.x + xPos, 0, transform.position.z + y * zOffset), Vector3.down, out hit, Mathf.Infinity, layerMask))
         {
-            Destroy(go.gameObject);
-            //If the start of the row is within the map bounds, start the next loop
-            //If the row is invalid, cancel the whole method
-            if (y != rowNum) { nextColumn = true; }
-            else { stop = true; }
+            //Because we are importing our models from Blender they have a -90 degree X rotation applied
+            //Because of this we need to use the "z" axis like we would use the "y" axis if we want "y" to continue to be our "height" dimension
+            //Because we are also offsetting our grid manager object we need to ensure the spawned hexes will originate from that new position and not the world coordinates
+            GameObject go = Instantiate(tileCoord, new Vector3(transform.position.x + xPos, 0, transform.position.z + y * zOffset), Quaternion.identity);
+            //We know each hex will have a script on it containing empty X and Y integers so when we instantiate a hex object, pass its X and Y data to each hex
+            HexData data = go.GetComponent<HexData>();
+            data.hexX = x;
+            data.hexY = y;
+            //Give each hex a name according to their grid coordinates and make them children of the grid master object for a cleaner heirarchy
+            go.name = "Hex (" + x + "," + y + ")";
+            go.transform.SetParent(transform);
+
+            //After spawning a valid tile, check if it is touching the map boundaries, if so, delete it
+            Collider[] hitColliders = Physics.OverlapSphere(go.transform.position, 0.5f, layerMask);
+            if (hitColliders.Length > 0)
+            {
+                Destroy(go.gameObject);
+            }
         }
     }
 
